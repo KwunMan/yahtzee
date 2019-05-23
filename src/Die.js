1435d190e32dd0d6093fc9a0834ee6b6e3f323bc
Luis Lo <kwun.man.lo@gmail.com>
2019-05-24 01:48:33 +0800

    css style updated and fixed restarting bug

---
+++ src/Die.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./Die.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix } from '@fortawesome/pro-solid-svg-icons';
+
+class Die extends Component {
+
+  static defaultProps = {
+    dieFace: [faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix],
+    val: 6
+  }  
+  lockDie = () => {
+    this.props.handleClick(this.props.idx);
+  }
+  render() {
+    const { locked, val, disabled, rolling, dieFace } = this.props;
+
+    return (
+      <FontAwesomeIcon 
+        disabled={disabled} 
+        icon={dieFace[val - 1]} 
+        size="4x" 
+        className={`Die ${locked ? "Die-locked" : ""} ${rolling ? "Die-rolling" : ""}`} 
+        onClick={this.lockDie}
+      />
+    );
+  }
+}
+
+export default Die;
